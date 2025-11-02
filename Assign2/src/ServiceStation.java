33b2ba4f8602923df32c7233b4b4a4657e086063
yossefabdelmoneim <yossefabdelmoneim9@gmail.com>
2025-11-03 01:00:02 +0200

    Override the run function

--- Assign2/src/ServiceStation.java
+++ Assign2/src/ServiceStation.java
@@ -44,6 +44,46 @@ class Car extends Thread {
         this.ID = ID ;
     }
     public int getID() { return ID ;}
+
+    @Override
+    public void run() {
+        try {
+            //Wait for an empty spot in the waiting area
+            Empty.Wait();
+            System.out.println("Car " + ID + " arrived at the service station.");
+
+            //Add car to the queue
+            Mutex.Wait();
+            queue.add(this);
+            System.out.println("Car " + ID + " is waiting in the queue. Queue size: " + queue.size());
+            Mutex.signal();
+
+            //Signal that there is a car or more waiting
+            Full.signal();
+
+            //Wait for an available pump
+            Pump.Wait();
+            System.out.println("Car " + ID + " is being serviced.");
+
+            //Simulate time taken to service the car
+            Thread.sleep(2000);
+
+            //Finish servicing
+            System.out.println("Car " + ID + " has been serviced and is leaving the station .");
+
+            //Free up the pump
+            Pump.signal();
+            Mutex.Wait();
+            queue.remove(this);
+            Mutex.signal();
+
+            //Signal that there is an empty spot in the waiting area
+            Empty.signal();
+
+        } catch (InterruptedException e) {
+            System.out.println("Error in the car: " + ID + ":" + e.getMessage());
+        }
+    }
 }
 
 public class ServiceStation {
